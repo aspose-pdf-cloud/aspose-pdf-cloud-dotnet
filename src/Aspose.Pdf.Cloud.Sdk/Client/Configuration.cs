24057c7694bf37ffe367e6c1802e068571cc5292
Andrey Kaferi <andrey.kaferi@aspose.com>
2020-12-15 16:46:28 +0200

    Update to v20.12

--- src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
@@ -222,7 +222,7 @@ namespace Aspose.Pdf.Cloud.Sdk.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: 3.0\n";
-            report += "    SDK Package Version: 20.9.0\n";
+            report += "    SDK Package Version: 20.12.0\n";
 
             return report;
         }
