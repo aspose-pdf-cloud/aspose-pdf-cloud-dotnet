7f6ab7817e912d0df0d214f3f7136e0d77a62289
Oleg Subachev <Oleg.Subachev@aspose.com>
2022-01-27 05:57:28 -0600

    year 2021 updated to 2022

--- src/Aspose.Pdf.Cloud.Sdk/Model/SplitResultResponse.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Model/SplitResultResponse.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright company="Aspose" file="SplitResultResponse.cs">
-//   Copyright (c) 2021 Aspose.PDF Cloud
+//   Copyright (c) 2022 Aspose.PDF Cloud
 // </copyright>
 // <summary>
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
