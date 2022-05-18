17a9de067f55a7287b2740573f0f360c45825ec8
Oleg Subachev <Oleg.Subachev@aspose.com>
2022-05-18 05:27:08 -0500

    communication with Cloud API token endpoint added

--- src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
@@ -60,8 +60,6 @@ namespace Aspose.Pdf.Cloud.Sdk.Client
         {
             if (string.IsNullOrEmpty(basePath))
                  throw new ArgumentException("basePath cannot be empty");
-            if (!_CheckSidKey(appSid, apiKey))
-                 throw new ArgumentException("appSid and apiKey are messed up or have wrong format");
             
             ApiKey = apiKey;
             AppSid = appSid;
@@ -72,13 +70,6 @@ namespace Aspose.Pdf.Cloud.Sdk.Client
             TempFolderPath = tempFolderPath;
             DateTimeFormat = dateTimeFormat;
             Timeout = timeout;
-        }
-
-        private static bool _CheckSidKey(string appSid, string apiKey)
-        {
-            var ssSid = appSid.Split('-');
-            var ssKey = apiKey.Split('-');
-            return ssSid.Length == 5 && ssKey.Length == 1;
         }
 
         /// <summary>
