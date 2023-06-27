bd1156ce1c1158cb1a33df074e06299b96035b35
Oleg Subachev <Oleg.Subachev@aspose.com>
2023-06-27 02:35:30 -0500

    update to 23.6

--- src/Aspose.Pdf.Cloud.Sdk/Client/ApiClient.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Client/ApiClient.cs
@@ -205,7 +205,7 @@ namespace Aspose.Pdf.Cloud.Sdk.Client
 
             // add custom header
             request.AddHeader(AsposeClientHeaderName, ".net sdk");
-            request.AddHeader(AsposeClientVersionHeaderName, "23.5.0");
+            request.AddHeader(AsposeClientVersionHeaderName, "23.6.0");
 
             // add header parameter, if any
             foreach(var param in headerParams)
