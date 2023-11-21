d59100e0a73a767714090c564f5f63ea2a8b118b
Oleg Subachev <Oleg.Subachev@aspose.com>
2023-11-21 03:31:57 -0600

    update to 23.11

--- src/Aspose.Pdf.Cloud.Sdk/Properties/AssemblyInfo.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.10.0")]
-[assembly: AssemblyFileVersion("23.10.0")]
+[assembly: AssemblyVersion("23.11.0")]
+[assembly: AssemblyFileVersion("23.11.0")]
