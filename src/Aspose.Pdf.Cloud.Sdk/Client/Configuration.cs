45f0df7d58bcd0ef8cca3c7e38163894c67c1aaa
Kirill Novinskiy <kirill.novinskiy@aspose.com>
2024-06-25 06:08:57 -0500

    update to 24.6

--- src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
+++ src/Aspose.Pdf.Cloud.Sdk/Client/Configuration.cs
@@ -225,7 +225,7 @@ namespace Aspose.Pdf.Cloud.Sdk.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: 3.0\n";
-            report += "    SDK Package Version: 24.5.0\n";
+            report += "    SDK Package Version: 24.6.0\n";
 
             return report;
         }
