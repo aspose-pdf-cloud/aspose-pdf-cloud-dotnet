a14d63438a3839e6686068d4d1f0a56564198920
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-11 12:16:31 +0300

    Update ChangeLayoutHelper.cs

--- Uses-Cases/ChangeLayout/ChangeLayoutHelper.cs
+++ Uses-Cases/ChangeLayout/ChangeLayoutHelper.cs
@@ -6,7 +6,7 @@ namespace ChangeLayout
 {
     public class ConfigParams
     {
-        public string CrdentialPath { get; } = "c:\\Projects\\ASPOSE\\Pdf.Cloud\\Credentials\\credentials.json ";
+        public string CrdentialPath { get; } = "..\\credentials.json ";
         public string LOCAL_FOLDER { get; } = "C:\\Samples";
         public string REMOTE_TEMP_FOLDER { get; } = "TempPdfCloud";
         public string PDF_DOCUMENT { get; } = "sample.pdf";
