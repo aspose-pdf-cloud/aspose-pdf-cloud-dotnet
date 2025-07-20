e61fb54c79da1bb1a2e76a512b9db36edf9cbc03
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-21 02:19:22 +0300

    PDFCLOUD-4997: added snippets for create pdf

---
+++ Uses-Cases/ChangeLayout/CreateDocument/ClreatePdfDocumentSimple.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Api;
+using Aspose.Pdf.Cloud.Sdk.Model;
+using RestSharp;
+
+namespace CreateDocument
+{
+    public class ClreatePdfDocumentSimple
+    {
+        public ClreatePdfDocumentSimple()
+        {
+            string LOCAL_FOLDER = "c:\\Samples";
+            string PDF_DOCUMENT = "output_created_simple.pdf";
+            string REMOTE_FOLDER = "TempPdfCloud";
+
+            string AppSID = "5ad16ee3-abef-40da-912e-0d5cf0beaabc";
+            string AppKey = "bc529468758ebc938e4c29a1569a6bd2";
+
+            PdfApi pdfApi = new PdfApi(AppKey, AppSID);
+
+            DocumentResponse response = pdfApi.PutCreateDocument(PDF_DOCUMENT, folder: REMOTE_FOLDER);
+            if (response.Code != 200)
+                Console.WriteLine("ClreatePdfDocumentSimple(): Unexpected error: {0}", response.Messages[0]);
+            else
+            {
+                Console.WriteLine("ClreatePdfDocumentSimple():Document '{0}' successfully created.", PDF_DOCUMENT);
+                Stream stream = pdfApi.DownloadFile(Path.Combine(REMOTE_FOLDER, PDF_DOCUMENT));
+                using var fileStream = File.Create(Path.Combine(LOCAL_FOLDER, PDF_DOCUMENT));
+                stream.Position = 0;
+                stream.CopyTo(fileStream);
+                Console.WriteLine("ClreatePdfDocumentSimple():Document '{0}' successfully downloaded.", PDF_DOCUMENT);
+            }
+        }
+    }
+}
---
+++ Uses-Cases/ChangeLayout/CreateDocument/CrateDocumentHelper.cs
@@ -0,0 +1,45 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Api;
+using Newtonsoft.Json;
+
+namespace CreateDocument
+{
+    public class ConfigParams
+    {
+        public string CrdentialPath { get; } = "c:\\Projects\\ASPOSE\\Pdf.Cloud\\Credentials\\credentials.json";
+        public string LOCAL_FOLDER { get; } = "C:\\Samples";
+        public string TEMP_FOLDER { get; } = "TempPdfCloud";
+        public string LOCAL_RESULT_DOCUMENT_NAME { get; } = "output_sample.pdf";
+        public int PAGE_WIDTH { get; } = 590;
+        public int PAGE_HEIGHT { get; } = 894;
+        public int PAGES_COUNT { get; } = 5;
+    }
+
+    public class Credentials
+    {
+        public string Id { get; set; }
+        public string Key { get; set; }
+    }
+
+    public class CrateDocumentHelper
+    {
+        public PdfApi pdfApi {  get; private set; }
+        public ConfigParams config { get; private set; }
+
+        public CrateDocumentHelper()
+        {
+            config = new ConfigParams();
+            string jsCredText = File.ReadAllText(config.CrdentialPath);
+            Credentials cred = JsonConvert.DeserializeObject<Credentials>(jsCredText);
+            pdfApi = new PdfApi(cred.Key, cred.Id);
+        }
+
+        public async void DownloadFile(string filename)
+        {
+            Stream stream = pdfApi.DownloadFile(Path.Combine(config.TEMP_FOLDER, filename));
+            using var fileStream = File.Create(Path.Combine(config.LOCAL_FOLDER, config.LOCAL_RESULT_DOCUMENT_NAME));
+            stream.Position = 0;
+            await stream.CopyToAsync(fileStream);
+            Console.WriteLine("DownloadFile(): File '{0}' successfully downloaded.", filename);
+        }
+    }
+}
