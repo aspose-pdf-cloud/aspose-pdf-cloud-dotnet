720b926761be9938486617af340859dc95aa8397
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-31 09:09:35 +0300

    PDFCLOUD-5026: added snippets for Links

---
+++ Uses-Cases/Links/LinksAdd.cs
@@ -0,0 +1,36 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Model;
+
+namespace Links
+{
+    public class LinksAdd
+    {
+        public static async Task Append(LinksHelper helper, string documentName, string outputName, int pageNumber, string LinkAction, string remoteFolder)
+        {
+            await helper.UploadFile(documentName);
+
+            Rectangle rectangle = new Rectangle(LLX: 238, LLY: 488.622, URX: 305, URY: 498.588);
+
+            Link link = new Link(LinkAction);
+
+            LinkAnnotation newLink = new LinkAnnotation(
+                new List<Link>() { link },
+                ActionType: LinkActionType.GoToURIAction,
+                Action: LinkAction,
+                Highlighting: LinkHighlightingMode.Invert,
+                Color: new Color(A: 0xFF, R: 0xAA, G: 0x00, B: 0x00),
+                Rect: rectangle
+            );
+
+            AsposeResponse response = await helper.pdfApi.PostPageLinkAnnotationsAsync(documentName, pageNumber, new List<LinkAnnotation>() { newLink }, folder: remoteFolder);
+
+            if (response == null)
+                Console.WriteLine("LinksAdd(): Unexpected error!");
+            else if (response.Code < 200 || response.Code > 299)
+                Console.WriteLine("LinksAdd(): Failed to add link to the document.");
+            else {
+                Console.WriteLine("LinksAdd(): link '{0}' successfully appended to the document '{1}.", LinkAction, documentName);
+                await helper.DownloadFile(documentName, outputName, "add_link_");
+            }
+        }
+    }
+}
---
+++ Uses-Cases/Links/LinksRemove.cs
@@ -0,0 +1,25 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Model;
+
+namespace Links
+{
+    public class LinksRemove
+    {
+        public static async Task Delete(LinksHelper helper, string documentName, string outputName, string LinkID, string remoteFolder)
+        {
+            await LinksExtractById.ShowInfo(helper, documentName, LinkID, remoteFolder);
+
+            await helper.UploadFile(documentName);
+
+            AsposeResponse response = await helper.pdfApi.DeleteLinkAnnotationAsync(documentName, LinkID, folder: remoteFolder);
+
+            if (response == null)
+                Console.WriteLine("LinksRemove(): Unexpected error!");
+            else if (response.Code < 200 || response.Code > 299)
+                Console.WriteLine("LinksRemove(): Failed to remove link from the document.");
+            else {
+                Console.WriteLine("LinksRemove(): link '{0}' successfully removed from the document '{1}.", LinkID, documentName);
+                await helper.DownloadFile(documentName, outputName, "del_link_");
+            }
+        }
+    }
+}
