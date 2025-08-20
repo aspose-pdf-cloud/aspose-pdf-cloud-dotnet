fa5eb84380dab7bbd4aba31d1d4f51991ceb1d7d
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-08-20 07:25:43 +0300

    PDFCLOUD-5115: added snippets for change passwords

---
+++ Uses-Cases/EnvryptDecrypt/EncryptPdf.cs
@@ -0,0 +1,32 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Model;
+
+namespace EncryptDecrypt
+{
+    public class EncryptPdf
+    {
+        public static async Task Encrypt(EncryptDecryptHelper helper, string documentName, string outputName, string remoteFolder)
+        {
+            await helper.UploadFile(documentName);
+
+            string userPassword = "User-Password";
+            string ownerPassword = "Owner-Password";
+
+            AsposeResponse response = await helper.pdfApi.PostEncryptDocumentInStorageAsync(
+                documentName,
+                helper.ToBase64(userPassword),
+                helper.ToBase64(ownerPassword),
+                CryptoAlgorithm.AESx256.ToString(),
+                folder: remoteFolder);
+
+            if (response == null)
+                Console.WriteLine("EncryptPdf(): Unexpected error!");
+            else if (response.Code < 200 || response.Code > 299)
+                Console.WriteLine("EncryptPdf(): Failed to encrypt document.");
+            else
+            {
+                Console.WriteLine("EncryptPdf(): document '{0} successfully encrypted.", documentName);
+                await helper.DownloadFile(documentName, outputName, "encrypt_");
+            }
+        }
+    }
+}
---
+++ Uses-Cases/EnvryptDecrypt/Program.cs
@@ -0,0 +1,11 @@
+﻿using EncryptDecrypt;
+using EnryptDecrypt;
+
+EncryptDecryptHelper helper = new EncryptDecryptHelper();
+ConfigParams config = helper.config;
+
+await EncryptPdf.Encrypt(helper, config.PDF_DOCUMENT, config.PDF_OUTPUT, config.REMOTE_TEMP_FOLDER);
+
+await DecryptPdf.Decrypt(helper, config.PDF_DOCUMENT_ENCRYPTED, config.PDF_OUTPUT, config.REMOTE_TEMP_FOLDER);
+
+await PasswordModify.Change(helper, config.PDF_DOCUMENT_ENCRYPTED, config.PDF_OUTPUT, config.REMOTE_TEMP_FOLDER);
