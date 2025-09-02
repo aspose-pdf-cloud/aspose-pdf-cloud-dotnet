b024366c80de700152bb28da66d93d52e3e37b7e
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-09-02 09:31:08 +0300

    PDFCLOUD-5126: added snippets Signatures

---
+++ Uses-Cases/Signatures/ReplaceSignature.cs
@@ -0,0 +1,44 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Model;
+
+namespace Signatures
+{
+    public class ReplaceSignature
+    {
+        public static async Task Update(SignaturesHelper helper, ConfigParams config, string documentName, string fieldName, string outputName, string remoteFolder)
+        {
+            await helper.UploadFile(documentName);
+
+            Signature signature = new Signature(
+                SignaturePath: config.SIGNATURE_PFX,
+                SignatureType: SignatureType.PKCS7,
+                Password: config.SIGNATURE_PASSWORD,
+                Contact: config.SIGNATURE_CONTACT,
+                Location: config.SIGNATURE_LOCATION,
+                Visible: true,
+                Rectangle: config.SIGNATURE_RECT,
+                FormFieldName: config.SIGNATURE_FORM_FIELD,
+                Authority: config.SIGNATURE_AUTHORITY,
+                Date: config.SIGNATURE_DATE,
+                ShowProperties: false);
+
+            SignatureField field = new SignatureField(
+                PageIndex: 1,
+                Signature: signature,
+                PartialName: fieldName,
+                Rect: config.SIGNATURE_RECT);
+
+            var response = await helper.pdfApi.PutSignatureFieldAsync(documentName, fieldName, field, folder: remoteFolder);
+
+            if (response == null)
+                Console.WriteLine("ReplaceSignature(): Unexpected error!");
+            else if (response.Code < 200 || response.Code > 299)
+                Console.WriteLine("ReplaceSignature(): Failed to replace Pdf document signature.");
+            else
+            {
+                Console.WriteLine("ReplaceSignature(): Signature replaced successfully in the Pdf document '{0}'.", documentName);
+                await helper.DownloadFile(documentName, outputName, string.Empty);
+            }
+        }
+
+    }
+}
---
+++ Uses-Cases/Signatures/SignaturesHelper.cs
@@ -0,0 +1,73 @@
+﻿using Aspose.Pdf.Cloud.Sdk.Api;
+using Aspose.Pdf.Cloud.Sdk.Model;
+using Newtonsoft.Json;
+
+namespace Signatures
+{
+    public class ConfigParams
+    {
+        public string CrdentialPath { get; } = "..\\credentials.json";
+        public string LOCAL_FOLDER { get; } = "C:\\Samples";
+        public string REMOTE_TEMP_FOLDER { get; } = "TempPdfCloud";
+        
+        public string PDF_DOCUMENT { get; } = "sample.pdf";
+        public string PDF_SIGNED_DOCUMENT { get; } = "sample-signed.pdf";
+
+        public string PDF_OUTPUT { get; } = "output_sample.pdf";
+
+        public string LOCAL_SIGNATURE_PATH { get; } = "C:\\Samples\\Signatures";
+        public string SIGNATURE_PFX { get; } = "Signatures.pfx";
+        public string SIGNATURE_FORM_FIELD { get; } = "Signature_1";
+        public string SIGNATURE_PASSWORD { get; } = YOUR_PASSWORD;
+        public string SIGNATURE_CONTACT { get; } = CERTIFICATE_CONTACT;
+        public string SIGNATURE_LOCATION { get; } = CERTIFICATE_LOCATION;
+        public string SIGNATURE_AUTHORITY { get; } = CERTIFICATE_ISSUER;
+        public string SIGNATURE_DATE { get; } = "08/25/2025 12:15:00.000 PM";
+        public Rectangle SIGNATURE_RECT { get; } = new Rectangle(100, 100, 500, 500);
+
+    }
+
+    public class Credentials
+    {
+        public string Id { get; set; }
+        public string Key { get; set; }
+    }
+
+    public class SignaturesHelper
+    {
+        public PdfApi pdfApi { get; private set; }
+        public ConfigParams config { get; private set; }
+
+        public SignaturesHelper()
+        {
+            config = new ConfigParams();
+            string jsCredText = File.ReadAllText(config.CrdentialPath);
+            Credentials cred = JsonConvert.DeserializeObject<Credentials>(jsCredText);
+            pdfApi = new PdfApi(cred.Key, cred.Id);
+        }
+
+        public async Task UploadFile(string fileName)
+        {
+            using (var file = File.OpenRead(Path.Combine(config.LOCAL_FOLDER, fileName)))
+            {
+                FilesUploadResult response = await pdfApi.UploadFileAsync(Path.Combine(config.REMOTE_TEMP_FOLDER, fileName), file);
+                if (response == null)
+                    Console.WriteLine("UploadFile(): Unexpected error - no response!");
+                else if (response.Errors != null && response.Errors.Count > 0)
+                    foreach (var error in response.Errors)
+                        Console.WriteLine("UploadFile(): {0} -> {1}", [error.Code, error.Message]);
+                else
+                    Console.WriteLine("UploadFile(): File '{0}' successfully uploaded.", fileName);
+            }
+        }
+
+        public async Task DownloadFile(string fileName, string outputName, string outputPrefix)
+        {
+            Stream stream = pdfApi.DownloadFile(Path.Combine(config.REMOTE_TEMP_FOLDER, fileName));
+            using var fileStream = File.Create(Path.Combine(config.LOCAL_FOLDER, outputPrefix + outputName));
+            stream.Position = 0;
+            await stream.CopyToAsync(fileStream);
+            Console.WriteLine("DownloadFile(): File '{0}' successfully downloaded.", outputPrefix + outputName);
+        }
+    }
+}
